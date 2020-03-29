963baf15b798a4695d5111438f1e13592fc76706
SK26 <saul.lazzarini@gmail.com>
2020-03-29 20:36:00 +0200

    Application completed and ready for deployment

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -2,10 +2,13 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import {
   GET_POSTS,
+  GET_POST,
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
-  ADD_POST
+  ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from "./types";
 
 //Get posts
@@ -82,6 +85,56 @@ export const addPost = formData => async dispatch => {
       payload: res.data
     });
     dispatch(setAlert("Post Created", "success"));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+//Get post
+export const getPost = id => async dispatch => {
+  try {
+    const res = await axios.get(`/api/posts/${id}`);
+    dispatch({
+      type: GET_POST,
+      payload: res.data
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+//Add comment
+export const addComment = (postId, formData) => async dispatch => {
+  try {
+    const res = await axios.post(`/api/posts/comment/${postId}`, formData);
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data
+    });
+    dispatch(setAlert("Comment Added", "success"));
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+//Delete comment
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId
+    });
+    dispatch(setAlert("Comment Removed", "success"));
   } catch (error) {
     dispatch({
       type: POST_ERROR,
